ed7851f5a8e285906219a6d2cd1f91288d726bd4
Nabendu Maiti <1206581+nmaiti@users.noreply.github.com>
2025-03-20 01:05:52 +0530

    ll update
    Signed-off-by: Nabendu Maiti <1206581+nmaiti@users.noreply.github.com>

--- linked_list.py
+++ linked_list.py
@@ -1,3 +1,5 @@
+import os 
+
 class Node:
     def __init__(self, data):
         self.data=data
@@ -36,15 +38,76 @@ class LinkedList:
         self.head = new_node
         return
 
+    def insertAtIndex(self, data, index):
+        if index == 0 :
+            self.insertAtBegin(data)
+            return
+     
+        cur_node = self.head
+        position = 0 # index
 
-llist = LinkedList()
-llist.printlist()
-llist.insert(1)
-llist.insert(5)
-llist.insert(2)
-llist.insert(4)
-llist.printlist()
-llist.insertAtBegin(24)
-llist.printlist()
+        while cur_node != None and position+1 != index:
+            position += 1
+            cur_node = cur_node.next
+            
+        if cur_node != None :
+            new_node=Node(data)
+            new_node.next=cur_node.next
+            cur_node.next=new_node
+        else :
+            print(f'Index_not_present')
+            return
+
+#    def deleteAtBegin(self):
+
+
+#     def deleteAtIndex(self, index):
+
+#     def deleteWithData(self, data):
+
+# def reverseList(self):
+
+# def mergeList(self, llist):
+
+# def removeDuplicates(self):
+
+# def findMiddle(self):
+
+# def findNthFromEnd(self, n):
+
+# def countOccurences(self, data):
+
+# def rotateList(self, k):
+
+# def isPalindrome(self):
+
+# def isCircular(self):
+
+
+
+def main():
+    llist = LinkedList()
+    llist.printlist()
+    llist.insert(1)
+    llist.insert(5)
+    llist.insert(2)
+    llist.insert(4)
+    llist.printlist()
+    llist.insertAtBegin(24)
+    llist.printlist()
+    llist.insertAtIndex(55,0)
+    llist.printlist()
+    llist.insertAtIndex(44,2)
+    llist.printlist()
+    llist.insertAtIndex(33,6)
+    llist.printlist()
+    return
+
+if __name__ == '__main__':
+    main()
+    print(f"{os.path.basename(__file__)} is called directly")
+else: 
+    print(f"{__file__} is being imported")
+
 
 
---
+++ main.py
@@ -0,0 +1,5 @@
+import linked_list
+
+if __name__ == '__main__':
+    linked_list.main()
+    
